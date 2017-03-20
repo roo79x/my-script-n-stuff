5b7ed10b80e4ef9b63d372b2bba7ad7e09999836
Roo79x <roo79x@gmail.com>
2017-03-21 00:17:29 +1030

    Add files via upload

---
+++ upgcln.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+#Title: Update script for KDE-Neon
+#Description: updates system
+#Author: Roo79x
+#Date: 20 March 2017
+#Usage: bash upgcln.sh
+##############################
+
+#output text colours
+txtblk=$(tput setaf 0) # Black
+txtred=$(tput setaf 1) # Red
+txtgrn=$(tput setaf 2) # Green
+txtylw=$(tput setaf 3) # Yellow
+txtblu=$(tput setaf 4) # Blue
+txtmag=$(tput setaf 5) # Magenta
+txtcyn=$(tput setaf 6) # Cyan
+txtwht=$(tput setaf 7) # White
+txtbld=$(tput bold) # start bold
+txtdim=$(tput dim) # Select dim (half-bright) mode
+txtsmul=$(tput smul) # Enable underline mode
+txtrmul=$(tput rmul) # Disable underline mode
+txtrev=$(tput rev) # Turn on reverse video mode
+txtsmso=$(tput smso) # Enter standout (bold) mode
+txtrmso=$(tput rmso) # Exit standout mode
+txtrst=$(tput sgr0) # Text reset
+
+greet="$(h=`date +%H`
+
+if [ $h -lt 12 ]; then
+  echo Good morning
+elif [ $h -lt 18 ]; then
+  echo Good afternoon
+else
+  echo Good evening
+fi)"
+
+user="${SUDO_USER:-$USER}"
+
+#Post Install
+echo "${txtbld} ${txtmag} "${greet}" "${user}" ${txtrst}"
+echo "${txtbld} ${txtblu} ${txtsmul}Running Update Script${txtrmul} ${txtrst}"
+echo "  "
+
+sudo -p "${txtbld} ${txtgrn} Please enter your password: ${txtrst}" whoami 1>/dev/null
+
+
+echo "${txtbld} ${txtund} ${txtgrn} Doing an Update, Upgrade and Cleanup... ${txtrst} ${txtcyn} [Cleanliness is next to Godliness you know!!] ${txtrst}"
+sudo apt-get -y update && sudo apt-get -y upgrade && sudo apt-get -y dist-upgrade && sudo apt-get -y autoclean && sudo apt-get -y autoremove --purge
+echo "   "
+
+
+#Finished
+echo "    "
+echo "${txtbld} ${txtred} END OF LINE... ${txtrst}"
+sleep 1s
+echo "   "
---
+++ useful-stuff.sh
@@ -0,0 +1,52 @@
+lsb_release -a
+
+---Linux mint green-------------------
+#ACCD8A --green
+-------------------------------------
+wget -nv --content-disposition
+
+------------------------------
+
+rm -rf ~/.local/share/Trash/files/*
+
+sudo gedit /usr/share/nautilus/ui/nautilus-navigation-window-ui.xml
+________________________________________________________________________
+
+
+gmail to
+https://mail.google.com/mail/u/0/?view=cm&fs=1&tf=1&source=mailto
+
+xubuntu lid switch do nothing when closed
+/etc/systemd/logind.conf
+
+Date time format for lightdm orage callendar etc
+%A %e %B %Y %l:%M %P
+
+
+crontab for orage calendar
+*/5 * * * * /home/roo79x/.local/share/orage/rays-orage-gcalsync.sh
+
+*/6 * * * * /home/roo79x/.local/share/orage/holidays-ics.sh
+
+mail watcher setup
+firefox -new-tab http://gmail.com
+
+/usr/bin/google-chrome-stable http://gmail.com
+
+notify-send "New mail" "You have new messages in your inbox" -i xfce-newmail
+
+###.gtkrc-2.0###
+style "whisker-menu-numix-dark-blue-theme"
+{
+base[NORMAL] = "#2B2B2B"
+base[ACTIVE] = "#4B5871"
+text[NORMAL] = "#ccc"
+text[ACTIVE] = "#fff"
+bg[NORMAL] = "#2B2B2B"
+bg[ACTIVE] = "#4B5871"
+bg[PRELIGHT] = "#4B5871"
+fg[NORMAL] = "#ccc"
+fg[ACTIVE] = "#fff"
+fg[PRELIGHT] = "#fff"
+}
+widget "whiskermenu-window*" style "whisker-menu-numix-dark-blue-theme"
