f9f9e4c41696a179a98ca26e5343b386d04fc6fd
Roo79x <roo79x@gmail.com>
2017-05-09 15:55:42 +0930

    Add files via upload

---
+++ xfce/delete-trash.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [ $# -eq 0 ]; then
+  rm -rf ~/.local/share/Trash/*/*
+else
+  for f in "$@"; 
+  do
+    rm -rf ~/.local/share/Trash/files/"$f"
+    rm -rf ~/.local/share/Trash/info/"$f".trashinfo 
+  done
+fi
+sleep 2s
+notify-send "Emty Trash" "Trash Has Been Emptied"
